89cf6d668e85a0cecaf8b76e959c7135732ae051
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-10-28 11:54:05 -0300

    Class number 02 of playlist - Introduction to arrays and vectors

---
+++ src/com/mmacedo/datastructure/vector/Vector.java
@@ -0,0 +1,10 @@
+package com.mmacedo.datastructure.vector;
+
+public class Vector {
+
+    private String[] elements;
+
+    public Vector(int length) {
+        this.elements = new String[length];
+    }
+}
