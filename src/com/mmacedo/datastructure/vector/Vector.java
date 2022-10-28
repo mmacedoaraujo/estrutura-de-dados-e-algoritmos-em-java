8f6244792e7c82e45f05d2e1ef70e3298a57a209
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-10-28 12:32:37 -0300

    Class number 03 of playlist - Adding element to the final of the vector

--- src/com/mmacedo/datastructure/vector/Vector.java
+++ src/com/mmacedo/datastructure/vector/Vector.java
@@ -3,8 +3,30 @@ package com.mmacedo.datastructure.vector;
 public class Vector {
 
     private String[] elements;
+    private int size;
 
     public Vector(int length) {
         this.elements = new String[length];
+        this.size = 0;
+    }
+
+   /* public void addNewElement(String element) {
+        for (int i = 0; i < elements.length; i++) {
+            if (this.elements[i] == null) {
+                this.elements[i] = element;
+                break;
+            }
+        }
+    }
+    */
+
+    public boolean addNewElement(String element) {
+        if (this.size < this.elements.length) {
+            this.elements[this.size] = element;
+            this.size++;
+            return true;
+        }
+        return false;
+
     }
 }
