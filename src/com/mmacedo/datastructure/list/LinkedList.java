348f2d781be62003ed646a7f2df997a9d22b1b33
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-16 11:43:13 -0300

    class number 27 of playlist - size of the list

--- src/com/mmacedo/datastructure/list/LinkedList.java
+++ src/com/mmacedo/datastructure/list/LinkedList.java
@@ -3,10 +3,16 @@ package com.mmacedo.datastructure.list;
 public class LinkedList<T> {
 
     private Node<T> begin;
+    private int size;
 
     public void addNewElement(T element) {
         Node<T> cell = new Node<T>(element);
         this.begin = cell;
+        this.size++;
+    }
+
+    public int getSize() {
+        return this.size;
     }
 
     @Override
---
+++ src/com/mmacedo/datastructure/list/classes_exercises/Class27.java
@@ -0,0 +1,15 @@
+package com.mmacedo.datastructure.list.classes_exercises;
+
+import com.mmacedo.datastructure.list.LinkedList;
+
+public class Class27 {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList<Integer>();
+
+        list.addNewElement(1);
+
+        System.out.println(list);
+        System.out.println(list.getSize());
+    }
+}
